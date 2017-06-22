9317e6d78dc045e6bc1d3b0a46a836cd93beb85d
Bruce <solomon.fc@gmail.com>
2017-06-22 13:51:56 +0800

    import

--- src/main/java/com/icbc/wfs/service/impl/WfsIOImpl.java
+++ src/main/java/com/icbc/wfs/service/impl/WfsIOImpl.java
@@ -1,10 +1,9 @@
 package com.icbc.wfs.service.impl;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
