e5a332dc1cfca66eb32f9bd00447ba731a545663
timmy <i@imtimmy.com>
2017-06-23 15:22:51 +0800

    env

--- src/main/java/com/icbc/wfs/WfsEnv.java
+++ src/main/java/com/icbc/wfs/WfsEnv.java
@@ -1,7 +1,23 @@
 package com.icbc.wfs;
 
+import com.icbc.dubbo.util.PropertyConfigurer;
+
 public class WfsEnv {
-    public static final String ROOT_DIR = System.getProperty("rootDir");
-    public static final String GROUP = System.getProperty("dubbo.service.group");
-    public static final String GROUP_FLAG = GROUP.substring(GROUP.lastIndexOf("-") + 1);
+	public static final String ROOT_DIR = getRootDir();
+	public static final String GROUP = getGroup();
+	public static final String GROUP_FLAG = getGroupFlag();
+
+	private static String getRootDir() {
+		return (String) PropertyConfigurer.getContextProperty("rootDir");
+	}
+
+	private static String getGroup() {
+		return (String) PropertyConfigurer.getContextProperty("dubbo.service.group");
+	}
+
+	private static String getGroupFlag() {
+		String group = (String) PropertyConfigurer.getContextProperty("dubbo.service.group");
+		return group.substring(group.lastIndexOf("-") + 1);
+	}
+
 }
