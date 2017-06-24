a78a59eb270595cd65a20872fb297ff4321fb37e
kfzx-wuzd <wuzd@sdc.icbc.com.cn>
2017-06-24 11:27:11 +0800

    并发问题

--- src/main/java/com/icbc/wfs/WfsUtil.java
+++ src/main/java/com/icbc/wfs/WfsUtil.java
@@ -5,69 +5,73 @@ import java.io.File;
 import com.icbc.dubbo.util.MurMurHash;
 
 public class WfsUtil {
-	public static final String PATH_SEPARATOR = "/";
-	public static final String ROOT = PATH_SEPARATOR;
+    public static final String PATH_SEPARATOR = "/";
+    public static final String ROOT = PATH_SEPARATOR;
 
-	public static String getFileName(String path) {
-		if (ROOT.equals(path)) {
-			return ROOT;
-		} else {
-			int sub;
-			if (path.endsWith(PATH_SEPARATOR)) {
-				sub = path.lastIndexOf(PATH_SEPARATOR, path.length() - 2);
-			} else {
-				sub = path.lastIndexOf(PATH_SEPARATOR);
-			}
-			return path.substring(sub + 1);
-		}
-	}
+    public static boolean isDirectory(String path) {
+        return path.endsWith(PATH_SEPARATOR);
+    }
 
-	public static String getParent(String path) {
-		if (ROOT.equals(path)) {
-			return ROOT;
-		} else {
-			int sub;
-			if (path.endsWith(PATH_SEPARATOR)) {
-				sub = path.lastIndexOf(PATH_SEPARATOR, path.length() - 2);
-			} else {
-				sub = path.lastIndexOf(PATH_SEPARATOR);
-			}
+    public static String getFileName(String path) {
+        if (ROOT.equals(path)) {
+            return ROOT;
+        } else {
+            int sub;
+            if (isDirectory(path)) {
+                sub = path.lastIndexOf(PATH_SEPARATOR, path.length() - 2);
+            } else {
+                sub = path.lastIndexOf(PATH_SEPARATOR);
+            }
+            return path.substring(sub + 1);
+        }
+    }
 
-			return path.substring(0, sub + 1);
-		}
-	}
+    public static String getParent(String path) {
+        if (ROOT.equals(path)) {
+            return ROOT;
+        } else {
+            int sub;
+            if (isDirectory(path)) {
+                sub = path.lastIndexOf(PATH_SEPARATOR, path.length() - 2);
+            } else {
+                sub = path.lastIndexOf(PATH_SEPARATOR);
+            }
 
-	public static String getPhyFilePath(String path) {
-		String hash = MurMurHash.hashRange(path);
-		return WfsEnv.ROOT_DIR + (WfsEnv.ROOT_DIR.endsWith(File.separator) ? "" : File.separator) + hash.substring(0, 2)
-				+ File.separator + hash;
-	}
+            return path.substring(0, sub + 1);
+        }
+    }
 
-	public static File getPhyFile(String path) {
-		return new File(getPhyFilePath(path));
-	}
+    public static String getPhyFilePath(String path) {
+        String hash = MurMurHash.hashRange(path);
+        return WfsEnv.ROOT_DIR + (WfsEnv.ROOT_DIR.endsWith(File.separator) ? "" : File.separator)
+                + hash.substring(0, 2) + File.separator + hash;
+    }
 
-	public static boolean delete(File targetFile) {
-		if (targetFile.isDirectory()) {
-			for (File file : targetFile.listFiles()) {
-				if (!delete(file)) {
-					return false;
-				}
-			}
-		} else {
-			while (targetFile.exists()) {
-				return targetFile.delete();
-			}
-		}
-		return true;
-	}
+    public static File getPhyFile(String path) {
+        return new File(getPhyFilePath(path));
+    }
 
-	public static boolean mergerFalse(boolean[] booleans) {
-		for (boolean b : booleans) {
-			if (!b) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static boolean delete(File targetFile) {
+        if (targetFile.isDirectory()) {
+            for (File file : targetFile.listFiles()) {
+                if (!delete(file)) {
+                    return false;
+                }
+            }
+        } else {
+            while (targetFile.exists()) {
+                return targetFile.delete();
+            }
+        }
+        return true;
+    }
+
+    public static boolean mergerFalse(boolean[] booleans) {
+        for (boolean b : booleans) {
+            if (!b) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/com/icbc/wfs/service/impl/WfsPutImpl.java
+++ src/main/java/com/icbc/wfs/service/impl/WfsPutImpl.java
@@ -10,6 +10,8 @@ import java.util.NoSuchElementException;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.dubbo.rpc.RpcContext;
@@ -20,77 +22,82 @@ import com.icbc.wfs.service.WfsPut;
 
 @Service("wfsPutImpl")
 public class WfsPutImpl implements WfsPut {
-	// private static Logger logger = LoggerFactory.getLogger(WfsPutImpl.class);
+    private static Logger logger = LoggerFactory.getLogger(WfsPutImpl.class);
 
-	@Resource
-	private WfsPut wfsPut;
-	@Resource
-	private WfsEditImpl wfsEditImpl;
+    @Resource
+    private WfsPut wfsPut;
+    @Resource
+    private WfsEditImpl wfsEditImpl;
 
-	public boolean put(String path, String flag, InputStream in) {
-		File phyFile = WfsUtil.getPhyFile(path);
-		if (!phyFile.getParentFile().exists()) {
-			if (!phyFile.getParentFile().mkdirs()) {
-				return false;
-			}
-		}
-		FileOutputStream out = null;
-		try {
-			out = new FileOutputStream(phyFile);
-			int n = -1;
-			byte[] b = new byte[0x2000];
-			while (-1 != (n = in.read(b))) {
-				out.write(b, 0, n);
-			}
-			out.flush();
-		} catch (Exception e) {
-			return false;
-		} finally {
-			try {
-				if (null != out) {
-					out.close();
-				}
-				if (null != in) {
-					in.close();
-				}
-			} catch (IOException e) {
-				return false;
-			}
-		}
+    public boolean put(String path, String flag, InputStream in) {
+        File phyFile = WfsUtil.getPhyFile(path);
+        if (!phyFile.getParentFile().exists()) {
+            if (!phyFile.getParentFile().mkdirs()) {
+                logger.error("put-->mkdir false");
+                return false;
+            }
+        }
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(phyFile);
+            int n = -1;
+            byte[] b = new byte[0x2000];
+            while (-1 != (n = in.read(b))) {
+                out.write(b, 0, n);
+            }
+            out.flush();
+        } catch (Exception e) {
+            logger.error("put-->Exception");
+            return false;
+        } finally {
+            try {
+                if (null != out) {
+                    out.close();
+                }
+                if (null != in) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                logger.error("put-->close Exception", e);
+                return false;
+            }
+        }
 
-		FileInputStream nextIn = null;
-		boolean ret = false;
-		try {
-			nextIn = new FileInputStream(phyFile);
-			ret = put0(path, flag, nextIn);
-		} catch (FileNotFoundException e) {
-			return false;
-		} finally {
-			try {
-				if (null != nextIn) {
-					nextIn.close();
-				}
-			} catch (IOException e) {
-				return false;
-			}
-		}
-		if (!ret) {
-			phyFile.delete();
-			return false;
-		}
-		return true;
-	}
+        FileInputStream nextIn = null;
+        boolean ret = false;
+        try {
+            nextIn = new FileInputStream(phyFile);
+            ret = put0(path, flag, nextIn);
+        } catch (FileNotFoundException e) {
+            return false;
+        } finally {
+            try {
+                if (null != nextIn) {
+                    nextIn.close();
+                }
+            } catch (IOException e) {
+                logger.error("put-->nextIn close Exception", e);
+                return false;
+            }
+        }
+        if (!ret) {
+            phyFile.delete();
+            return false;
+        }
+        return true;
+    }
 
-	public boolean put0(String path, String flag, InputStream in) {
-		RpcContext.getContext().setAttachment(WfsRouter.ROUTE_KEY, path);
-		flag = flag.concat(WfsEnv.GROUP_FLAG);
-		RpcContext.getContext().setAttachment(WfsRouter.ROUTE_FLAG, flag);
-		try {
-			return wfsPut.put(path, flag, in);
-		} catch (NoSuchElementException e) {
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+    public boolean put0(String path, String flag, InputStream in) {
+        RpcContext.getContext().setAttachment(WfsRouter.ROUTE_KEY, path);
+        flag = flag.concat(WfsEnv.GROUP_FLAG);
+        RpcContext.getContext().setAttachment(WfsRouter.ROUTE_FLAG, flag);
+        try {
+            return wfsPut.put(path, flag, in);
+        } catch (NoSuchElementException e) {
+            return true;
+        } catch (Exception e) {
+            logger.error("put0--> Exception", e);
+            return false;
+        }
+    }
 }
