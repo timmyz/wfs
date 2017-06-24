2730192738fbd252e4b7375287fd4ff226237556
Bruce <solomon.fc@gmail.com>
2017-06-24 12:50:15 +0800

    fix WfsRestorer

--- src/main/java/com/icbc/wfs/WfsRestorer.java
+++ src/main/java/com/icbc/wfs/WfsRestorer.java
@@ -1,34 +1,29 @@
 package com.icbc.wfs;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.icbc.dubbo.router.HashRouter;
-import com.icbc.dubbo.router.WfsRouter;
 import com.icbc.wfs.service.WfsGet;
-import com.icbc.wfs.service.impl.WfsGetImpl;
+import com.icbc.wfs.service.impl.WfsPutImpl;
 
 public class WfsRestorer {
 
-	private static WfsRestorer instance;
+	private static Logger logger = LoggerFactory.getLogger(WfsRestorer.class);
 
 	@Resource
 	private WfsGet wfsGet;
-
-	private WfsRestorer() {
-	}
-
-	public static synchronized WfsRestorer getInstance() {
-		if (instance == null) {
-			instance = new WfsRestorer();
-		}
-		return instance;
-	}
+	private WfsPutImpl wfsPutImpl;
 
 	public void doRestore() {
 
@@ -73,14 +68,41 @@ public class WfsRestorer {
 						}
 					}
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					logger.error("put-->false");
 				}
 
 				for (String phyFilePath : phyFileList) {
 					RpcContext.getContext().setAttachment(HashRouter.ROUTE_VALUE, phyFilePath);
-					// 本地物理路径=根目录+哈希
-					wfsGet.getPhy(phyFilePath);
+					File phyFile = WfsUtil.getPhyFile(WfsEnv.ROOT_DIR + phyFilePath);
+
+					if (!phyFile.exists()) {
+
+						InputStream stream = wfsGet.getPhy(phyFilePath);
+						FileOutputStream out = null;
+						try {
+							out = new FileOutputStream(phyFile);
+							int n = -1;
+							byte[] b = new byte[0x2000];
+							while (-1 != (n = stream.read(b))) {
+								out.write(b, 0, n);
+							}
+							out.flush();
+						} catch (Exception e) {
+							logger.error("put-->Exception");
+						} finally {
+							try {
+								if (null != out) {
+									out.close();
+								}
+								if (null != stream) {
+									stream.close();
+								}
+							} catch (IOException e) {
+								logger.error("put-->close Exception", e);
+							}
+						}
+					}
+
 				}
 			}
 		}
