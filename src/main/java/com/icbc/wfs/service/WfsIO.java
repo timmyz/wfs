02385da3b79b06b2c7da623831fa3fe32214c990
Bruce <solomon.fc@gmail.com>
2017-06-26 19:14:48 +0800

    add interface comments

--- src/main/java/com/icbc/wfs/service/WfsIO.java
+++ src/main/java/com/icbc/wfs/service/WfsIO.java
@@ -4,14 +4,41 @@ import java.io.InputStream;
 import java.util.List;
 
 public interface WfsIO {
-
+	
+	/**
+	 * 存放文件
+	 * @param path 虚拟路径
+	 * @param in 文件流
+	 * @return 成功失败
+	 */
 	boolean put(String path, InputStream in);
 
+	/**
+	 * 获取文件
+	 * @param path 文件流
+	 * @return 文件流
+	 */
 	InputStream get(String path);
-
+	
+	/**
+	 * 重命名文件
+	 * @param newPath
+	 * @param oldPath
+	 * @return 成功失败
+	 */
 	boolean ren(String newPath, String oldPath);
-
+	
+	/**
+	 * 删除文件
+	 * @param path 虚拟路径
+	 * @return 成功失败
+	 */
 	boolean del(String path);
-
+	
+	/**
+	 * 获取目录
+	 * @param path 虚拟路径
+	 * @return 文件列表
+	 */
 	List<String> list(String path);
 }
