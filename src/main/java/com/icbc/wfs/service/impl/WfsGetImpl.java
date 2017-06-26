a7a4ba910c41ba5935464625c62fcc2ffac53aa3
kfzx-wuzd <wuzd@sdc.icbc.com.cn>
2017-06-26 21:04:14 +0800

    list

--- src/main/java/com/icbc/wfs/service/impl/WfsGetImpl.java
+++ src/main/java/com/icbc/wfs/service/impl/WfsGetImpl.java
@@ -20,79 +20,81 @@ import com.icbc.wfs.service.WfsGet;
 // @Service(version = "0.0.1")
 @Service("wfsGetImpl")
 public class WfsGetImpl implements WfsGet {
-	private static Logger logger = LoggerFactory.getLogger(WfsGetImpl.class);
+    private static Logger logger = LoggerFactory.getLogger(WfsGetImpl.class);
 
-	@Override
-	public InputStream get(String path) {
-		return getPhy0(WfsUtil.getPhyFile(path));
-	}
+    @Override
+    public InputStream get(String path) {
+        return getPhy0(WfsUtil.getPhyFile(path));
+    }
 
-	@Override
-	public InputStream getPhy(String path) {
-		return getPhy0(new File(WfsUtil.getPhyFilePathByHash(path)));
-	}
+    @Override
+    public InputStream getPhy(String path) {
+        return getPhy0(new File(WfsUtil.getPhyFilePathByHash(path)));
+    }
 
-	private InputStream getPhy0(File file) {
-		BufferedInputStream fin = null;
-		try {
-			fin = new BufferedInputStream(new FileInputStream(file));
-		} catch (FileNotFoundException e) {
-			logger.warn("getPhy0-->FileNotFoundException" + file.getName(), e);
-			throw new RpcException(RpcException.FORBIDDEN_EXCEPTION, "FileNotFoundException" + file.getName(), e);
-		}
-		return fin;
-	}
+    private InputStream getPhy0(File file) {
+        BufferedInputStream fin = null;
+        try {
+            fin = new BufferedInputStream(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            logger.warn("getPhy0-->FileNotFoundException" + file.getName(), e);
+            throw new RpcException(RpcException.FORBIDDEN_EXCEPTION,
+                    "FileNotFoundException" + file.getName(), e);
+        }
+        return fin;
+    }
 
-	/**
-	 * 物理层实现ls命令，获取文件列表
-	 */
-	@Override
-	public List<String> getList(String path) {
-		File dir = WfsUtil.getPhyFile(path);
-		if (!dir.exists() && !dir.isDirectory()) {
-			logger.warn("getList-->not exists or is directory" + path);
-			throw new RpcException(RpcException.FORBIDDEN_EXCEPTION);
-		}
-		List<String> fileList = new LinkedList<String>();
-		File[] fileArray = dir.listFiles();
-		if (fileArray != null) {
-			for (int i = 0; i < fileArray.length; i++) {
-				fileList.add(fileArray[i].getName());
-			}
-		}
-		return fileList;
-	}
+    /**
+     * 物理层实现ls命令，获取文件列表
+     */
+    @Override
+    public List<String> getList(String path) {
+        File dir = WfsUtil.getPhyFile(path);
+        if (!dir.exists() && !dir.isDirectory()) {
+            logger.warn("getList-->not exists or is directory" + path);
+            throw new RpcException(RpcException.FORBIDDEN_EXCEPTION);
+        }
+        List<String> fileList = new LinkedList<String>();
+        File[] fileArray = dir.listFiles();
+        if (fileArray != null) {
+            for (File file : fileArray) {
+                fileList.add(WfsUtil.getName(file));
+            }
+        }
+        return fileList;
+    }
 
-	/*
-	 * 递归获取目录下所有文件及文件夹
-	 */
-	@Override
-	public List<String> getPhyList(String path) {
-		List<String> fileList = new LinkedList<String>();
-		File dir = new File(WfsEnv.ROOT_DIR + path);
-		if (dir.exists() && dir.isDirectory()) {
-			File[] fileArr = dir.listFiles();
-			if (fileArr == null) {
-				return fileList;
-			}
-			for (File file : fileArr) {
-				if (!WfsUtil.isHash(file.getName())) {
-					logger.error("getPhyList:" + file.getPath() + " is not valid");
-					continue;
-				}
+    /*
+     * 递归获取目录下所有文件及文件夹
+     */
+    @Override
+    public List<String> getPhyList(String path) {
+        List<String> fileList = new LinkedList<String>();
+        File dir = new File(WfsEnv.ROOT_DIR + path);
+        if (dir.exists() && dir.isDirectory()) {
+            File[] fileArr = dir.listFiles();
+            if (fileArr == null) {
+                return fileList;
+            }
+            for (File file : fileArr) {
+                if (!WfsUtil.isHash(file.getName())) {
+                    logger.error("getPhyList:" + file.getPath() + " is not valid");
+                    continue;
+                }
 
-				fileList.add(WfsUtil.getName(file));
-				if (file.isDirectory()) {
-					File[] subFileArr = file.listFiles();
-					if (subFileArr == null) {
-						continue;
-					}
-					for (File subFile : subFileArr) {
-						fileList.add(file.getName() + WfsUtil.PATH_SEPARATOR + WfsUtil.getName(subFile));
-					}
-				}
-			}
-		}
-		return fileList;
-	}
+                fileList.add(WfsUtil.getName(file));
+                if (file.isDirectory()) {
+                    File[] subFileArr = file.listFiles();
+                    if (subFileArr == null) {
+                        continue;
+                    }
+                    for (File subFile : subFileArr) {
+                        fileList.add(
+                                file.getName() + WfsUtil.PATH_SEPARATOR + WfsUtil.getName(subFile));
+                    }
+                }
+            }
+        }
+        return fileList;
+    }
 }
