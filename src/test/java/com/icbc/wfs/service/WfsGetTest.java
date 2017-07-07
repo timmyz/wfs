a554d830069a9e051281c0001c7274550dab24b5
kfzx-wuzd <wuzd@sdc.icbc.com.cn>
2017-07-07 08:53:46 +0800

    test

--- src/test/java/com/icbc/wfs/service/WfsGetTest.java
+++ src/test/java/com/icbc/wfs/service/WfsGetTest.java
@@ -2,18 +2,33 @@ package com.icbc.wfs.service;
 
 import java.util.List;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 
 public class WfsGetTest {
 
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext_test.xml");
-        WfsGet wfsGet = context.getBean(WfsGet.class);
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("wfs-test");
+        RegistryConfig registry = new RegistryConfig();
+        registry.setProtocol("zookeeper");
+        registry.setAddress("122.26.13.124:2181,122.26.13.125:2181,122.26.13.126:2181");
+        registry.setCheck(false);
+
+        ReferenceConfig<WfsGet> reference = new ReferenceConfig<WfsGet>();
+        reference.setApplication(application);
+        reference.setTimeout(300000);
+        reference.setRegistry(registry);
+        reference.setCheck(false);
+        reference.setInterface(WfsGet.class);
+        reference.setGroup("*");
+
+        WfsGet wfsGet = reference.get();
+
         List<String> list = wfsGet.getPhyList("62", "");
         for (String s : list) {
             System.out.println(s);
         }
-        context.close();
     }
 }
--- src/test/java/com/icbc/wfs/service/WfsIOTest.java
+++ src/test/java/com/icbc/wfs/service/WfsIOTest.java
@@ -5,23 +5,40 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 
 public class WfsIOTest {
 
+    // TODO Auto-generated method stub
+    // public static final String inputPath = "/Users/timmyzhang/Downloads/Dash.zip";
+    public static final String inputPath = "B:\\Temp\\NOTES_9.0.1_MAC_64B_ZHCN_ZHTW.dmg";
+    // public static final String inputPath = "";
+
+    // public static final String outputPath = "/Users/timmyzhang/temp/output/Dash.zip";
+    public static final String outputPath = "B:\\NOTES_9.0.1_MAC_64B_ZHCN_ZHTW.dmg";
+    // public static final String outputPath = "";
+
     public static void main(String[] args) throws Exception {
-        // TODO Auto-generated method stub
-        // String inputPath = "/Users/timmyzhang/Downloads/Dash.zip";
-        String inputPath = "B:\\Temp\\Chrysanthemum.jpg";
-        // String inputPath = "";
 
-        // String outputPath = "/Users/timmyzhang/temp/output/Dash.zip";
-        String outputPath = "B:\\Chrysanthemum.jpg";
-        // String outputPath = "";
+        ApplicationConfig application = new ApplicationConfig();
+        application.setName("wfs-test");
+        RegistryConfig registry = new RegistryConfig();
+        registry.setProtocol("zookeeper");
+        registry.setAddress("122.26.13.124:2181,122.26.13.125:2181,122.26.13.126:2181");
+        registry.setCheck(false);
 
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext_test.xml");
-        WfsIO wfsIO = context.getBean(WfsIO.class);
+        ReferenceConfig<WfsIO> reference = new ReferenceConfig<WfsIO>();
+        reference.setApplication(application);
+        reference.setTimeout(300000);
+        reference.setRegistry(registry);
+        reference.setCheck(false);
+        reference.setInterface(WfsIO.class);
+        reference.setVersion("0.0.1");
+
+        WfsIO wfsIO = reference.get();
+
         boolean fst = true;
         while (true) {
             if (fst || '\n' == System.in.read()) {
@@ -74,6 +91,5 @@ public class WfsIOTest {
 
             }
         }
-        context.close();
     }
 }
